df0de36bbcf3b534484e20670e726d707021e4fb
Domas Sestakovas <sestakovasdomas@gmail.com>
2023-11-11 16:25:34 +0200

    props

--- App.js
+++ App.js
@@ -6,11 +6,16 @@ import Entype from 'react-native-vector-icons/Entypo';
 import HomeSceen from './src/screens/Home';
 import Post from './src/components/Post';
 
+import feed from './assets/data/feed';
+
+const post1 = feed[0];
+const post2 = feed[2];
+
 const App = () => {
   return (
     <SafeAreaView>
       {/*<HomeSceen />*/}
-      <Post />
+      <Post post={post2}/>
     </SafeAreaView>
   );
 };
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,25 +2,28 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
-  return(
+const Post = (props) => {
+
+  const post = props.post;
+
+  return (
     <View style={styles.container}>
       <Image
         style={styles.image}
-        source={{uri: 'https://picsum.photos/200/300'}}
+        source={{uri: post.image}}
       />
-      <Text style={styles.bedroom}>1 Bed 1 Bathroom</Text>
+      <Text style={styles.bedroom}>{post.bed} bed {post.bedroom} bedroom</Text>
 
-      <Text style={styles.description} numberOfLines={2}>Lorem ipsum travli vali ansk faksf nask fnamfkas kfna kasn fkas fnask fnkas nis fnas knfaks nfkasf n</Text>
-
-      <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>34€</Text>
-        <Text style={styles.price}>  43€ </Text>
-        / night
+      <Text style={styles.description} numberOfLines={2}>
+        {post.type}. {post.title}
       </Text>
 
-
-      <Text style={styles.totalPrice}>212€ total</Text>
+      <Text style={styles.prices}>
+        <Text style={styles.oldPrice}>{post.oldPrice}€</Text>
+        <Text style={styles.price}>  {post.newPrice}€ </Text>
+        / night
+      </Text>
+      <Text style={styles.totalPrice}>{post.totalPrice}€ total</Text>
     </View>
   );
 };
